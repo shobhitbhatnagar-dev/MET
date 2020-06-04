8d86799e7d9054a753304391235df5fd4e047b45
shobhit2610 <mailsbhatnagar@gmail.com>
2020-06-05 00:42:05 +0530

    Initial Commit

---
+++ MET.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using MET.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MET.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+       
+       public DbSet<Value> Values { get; set; }
+    }
+}
